9175cbcc528178a2874167a0039077d62cefc0f4
daehyoungcho <daehyoung.cho@gmail.com>
2020-06-29 23:19:43 +0900

    first commit

---
+++ src/test/java/kr/luxsoft/config/DemoUnitTestApplicationTests.java
@@ -0,0 +1,25 @@
+package kr.luxsoft.config;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class DemoUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	void test() {
+		
+	}
+
+}
